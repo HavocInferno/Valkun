a438bd33403c852a6ca0ab906831cf0e1ba026da
Paul Preissner <preissner.paul@gmail.com>
2019-02-01 13:53:01 +0100

    up to [080]

--- src/runCompiler.bat
+++ src/runCompiler.bat
@@ -1,3 +1,2 @@
-C:\VulkanSDK\1.1.82.1\Bin\glslangValidator.exe -V shader.vert
-C:\VulkanSDK\1.1.82.1\Bin\glslangValidator.exe -V shader.frag
-pause
+C:\VulkanSDK\1.1.85.0\Bin\glslangValidator.exe -V shader.vert
+C:\VulkanSDK\1.1.85.0\Bin\glslangValidator.exe -V shader.frag
